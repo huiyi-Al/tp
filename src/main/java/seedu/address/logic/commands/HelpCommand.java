675f76ed1728cc12e422cd2d3b091faa0af21a4a
huiyi-Al <E1295583@u.nus.edu>
2025-10-11 19:07:02 +0800

    Update HelpCommand.java

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -12,7 +12,10 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE = "Available commands:\n" 
+            + "addclient, editclient, deleteclient, listclients, " 
+            + "searchclient, addbooking, editbooking, viewbookings, "
+            + "tagbooking, viewclient, markbooking, help, exit";
     @Override
     public CommandResult execute(Model model) {
         return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
