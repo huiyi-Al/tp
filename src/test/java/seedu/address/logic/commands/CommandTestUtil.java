b7ad65a1b3ba065458b93a47306b90f4927f3e08
Jaejun Shim <jaejun.shim@outlook.com>
2026-03-16 16:33:36 +0800

    Update constraints for Notes field

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -58,7 +58,7 @@ public class CommandTestUtil {
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_NOTES_DESC = " " + PREFIX_NOTES
-            + "first line\nsecond line"; // newline not allowed for notes
+            + "a".repeat(201); // notes should be at most 200 chars
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
