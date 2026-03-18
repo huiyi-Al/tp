1f484f61434a73e0dfd0a4dce496ee9b29dcfcb3
huiyi-Al <E1295583@u.nus.edu>
2026-03-18 19:26:10 +0800

    bug fix

--- src/main/java/seedu/address/ui/PersonDetailPanel.java
+++ src/main/java/seedu/address/ui/PersonDetailPanel.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 
