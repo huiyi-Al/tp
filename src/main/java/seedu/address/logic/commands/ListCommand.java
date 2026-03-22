194cdfa3dc8de422dccf0c190f702b2004a3d686
kihoo56tkr <161791642+kihoo56tkr@users.noreply.github.com>
2026-03-22 15:21:09 +0800

    fix formatting error

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -12,7 +12,8 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows a sorted list of all persons in the address book.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Shows a sorted list of all persons in the address book.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
