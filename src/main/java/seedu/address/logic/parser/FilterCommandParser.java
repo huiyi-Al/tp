d84d9c6d8d660e4aafa88659de4d1652129a570b
Sheryl Phoa <sherylphoa18@gmail.com>
2026-03-23 17:08:16 +0800

    Add logic and predicate for filter

---
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.List;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.predicates.TagsMatchKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FilterCommand object
+ */
+public class FilterCommandParser implements Parser<FilterCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FilterCommand
+     * and returns a FilterCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public FilterCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
+        List<String> tagKeywords = argMultimap.getAllValues(PREFIX_TAG);
+
+        // Ensure at least one --tag= prefix was provided.
+        boolean isTagListEmpty = tagKeywords.isEmpty();
+
+        // Ensure the preamble is empty.
+        boolean hasPreamble = !argMultimap.getPreamble().isEmpty();
+
+        // Ensure no provided tag is empty or just whitespace
+        boolean hasBlankTags = tagKeywords.stream().anyMatch(tag -> tag.trim().isEmpty());
+
+        if (isTagListEmpty || hasPreamble || hasBlankTags) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+        }
+
+        return new FilterCommand(new TagsMatchKeywordsPredicate(tagKeywords));
+    }
+}
