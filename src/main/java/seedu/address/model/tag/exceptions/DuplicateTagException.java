aff247294cdeec2cccf534e7b9ee5b6ec2884ce2
Sheryl Phoa <sherylphoa18@gmail.com>
2026-03-24 17:47:19 +0800

    Add UniqueTagList and exceptions for tag

---
+++ src/main/java/seedu/address/model/tag/exceptions/DuplicateTagException.java
@@ -0,0 +1,10 @@
+package seedu.address.model.tag.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Tags (Tags are considered duplicates if they are equal).
+ */
+public class DuplicateTagException extends RuntimeException {
+    public DuplicateTagException() {
+        super("Operation would result in duplicate tags");
+    }
+}
