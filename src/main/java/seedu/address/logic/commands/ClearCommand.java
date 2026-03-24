6af18aa14dbdf6d74ead8892cf88a3c7aa512546
kihoo56tkr <161791642+kihoo56tkr@users.noreply.github.com>
2026-03-24 17:45:06 +0800

    Edit message_usage to be clearer

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -13,6 +13,7 @@ public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears all entries from the address book.\n"
+            + "Note: this command does not accept any arguments.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
