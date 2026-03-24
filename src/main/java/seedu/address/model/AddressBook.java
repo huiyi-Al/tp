fcc7a1a359a2cb1d2cfd05d8d62bb6dc8af74f05
Sheryl Phoa <sherylphoa18@gmail.com>
2026-03-24 22:50:39 +0800

    Update AddressBook and ReadOnlyAddressBook to hold the UniqueTagList

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -2,12 +2,18 @@ package seedu.address.model;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
 
 /**
  * Wraps all data at the address-book level
@@ -16,6 +22,7 @@ import seedu.address.model.person.UniquePersonList;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
+    private final UniqueTagList tags;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -26,6 +33,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     {
         persons = new UniquePersonList();
+        tags = new UniqueTagList();
     }
 
     public AddressBook() {}
@@ -49,12 +57,21 @@ public class AddressBook implements ReadOnlyAddressBook {
     }
 
     /**
+     * Replaces the contents of the tag list with {@code tags}.
+     * {@code tags} must not contain duplicate tags.
+     */
+    public void setTags(List<Tag> tags) {
+        this.tags.setTags(tags);
+    }
+
+    /**
      * Resets the existing data of this {@code AddressBook} with {@code newData}.
      */
     public void resetData(ReadOnlyAddressBook newData) {
         requireNonNull(newData);
 
         setPersons(newData.getPersonList());
+        setTags(newData.getTagList());
     }
 
     //// person-level operations
@@ -64,6 +81,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     public boolean hasPerson(Person person) {
         requireNonNull(person);
+
         return persons.contains(person);
     }
 
@@ -73,6 +91,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     public void addPerson(Person p) {
         persons.add(p);
+        updateTagList(p);
     }
 
     /**
@@ -84,6 +103,7 @@ public class AddressBook implements ReadOnlyAddressBook {
         requireNonNull(editedPerson);
 
         persons.setPerson(target, editedPerson);
+        updateTagList(editedPerson);
     }
 
     /**
@@ -94,18 +114,88 @@ public class AddressBook implements ReadOnlyAddressBook {
         persons.remove(key);
     }
 
+    //// tag-level operations
+
+    /**
+     * Returns true if a tag equal to {@code tag} exists in the address book.
+     */
+    public boolean hasTag(Tag tag) {
+        requireNonNull(tag);
+
+        return tags.contains(tag);
+    }
+
+    /**
+     * Updates the tag {@code target} to {@code editedTag} globally.
+     * Every person currently holding {@code target} will be updated to hold {@code editedTag}.
+     */
+    public void setTag(Tag target, Tag editedTag) {
+        requireNonNull(target);
+        requireNonNull(editedTag);
+
+        tags.setTag(target, editedTag);
+
+        List<Person> updatedPersonList = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getTags().contains(target)) {
+                updatedPersonList.add(createUpdatedPersonWithNewTag(person, target, editedTag));
+            } else {
+                updatedPersonList.add(person);
+            }
+        }
+
+        persons.setPersons(updatedPersonList);
+    }
+
+    /**
+     * Helper method to create a new Person instance with the {@code editedTag}.
+     * Maintains immutability of the Person class.
+     */
+    private Person createUpdatedPersonWithNewTag(Person person, Tag target, Tag editedTag) {
+        Set<Tag> updatedTags = new HashSet<>(person.getTags());
+        updatedTags.remove(target);
+        updatedTags.add(editedTag);
+
+        return new Person(
+                person.getName(),
+                person.getPhone(),
+                person.getEmail(),
+                person.getAddress(),
+                person.getNotes(),
+                updatedTags
+        );
+    }
+
+    /**
+     * Updates the internal UniqueTagList when a person is added or edited to ensure that the internal UniqueTagList
+     * contains all tags present in a person.
+     */
+    private void updateTagList(Person person) {
+        person.getTags().forEach(tag -> {
+            if (!tags.contains(tag)) {
+                tags.add(tag);
+            }
+        });
+    }
+
     //// util methods
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
                 .add("persons", persons)
+                .add("tags", tags)
                 .toString();
     }
 
     @Override
     public ObservableList<Person> getPersonList() {
         return persons.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public ObservableList<Tag> getTagList() {
+        return tags.asUnmodifiableObservableList();
     }
 
     @Override
@@ -120,11 +210,12 @@ public class AddressBook implements ReadOnlyAddressBook {
         }
 
         AddressBook otherAddressBook = (AddressBook) other;
-        return persons.equals(otherAddressBook.persons);
+        return persons.equals(otherAddressBook.persons)
+                && tags.equals(otherAddressBook.tags);
     }
 
     @Override
     public int hashCode() {
-        return persons.hashCode();
+        return Objects.hash(persons, tags);
     }
 }
