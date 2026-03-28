f36c8006f776532f30286ba816cbbb4145f89453
kihoo56tkr <161791642+kihoo56tkr@users.noreply.github.com>
2026-03-28 23:42:35 +0800

    Edit delete message_usage to be consistent with other commands using index

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -24,7 +24,7 @@ public class DeleteCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1\n"
             + "Note: You will be prompted to confirm the deletion by typing the command again.";
 
