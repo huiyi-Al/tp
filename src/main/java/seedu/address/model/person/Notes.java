eca7b32543085f9cca693722421c14316cada73e
Jaejun Shim <jaejun.shim@outlook.com>
2026-03-30 17:49:53 +0800

    Improve Notes constraints message to be more aligned

--- src/main/java/seedu/address/model/person/Notes.java
+++ src/main/java/seedu/address/model/person/Notes.java
@@ -11,7 +11,8 @@ public class Notes {
 
     public static final String DEFAULT_NOTE = "";
     public static final int MAX_LENGTH = 200;
-    public static final String MESSAGE_CONSTRAINTS = "Notes should be between 0 and 200 characters.";
+    public static final String MESSAGE_CONSTRAINTS =
+            "Notes should be between 0 and 200 characters (measured in Unicode code points).";
 
     public final String value;
 
