8334c3a7218b5f03b4cf7171f20c649cc109070a
Jaejun Shim <jaejun.shim@outlook.com>
2026-03-31 13:51:38 +0800

    Refactor logdelete parser to simplify ParseException flow

--- src/main/java/seedu/address/logic/parser/LogDeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LogDeleteCommandParser.java
@@ -19,23 +19,26 @@ public class LogDeleteCommandParser implements Parser<LogDeleteCommand> {
      */
     public LogDeleteCommand parse(String args) throws ParseException {
         requireNonNull(args);
+        String usageMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, LogDeleteCommand.MESSAGE_USAGE);
 
+        String trimmedArgs = args.trim();
+        String[] parts = trimmedArgs.split("\\s+");
+
+        if (parts.length != 2) {
+            throw new ParseException(usageMessage);
+        }
+
+        Index personIndex = parseIndexOrThrowUsage(parts[0], usageMessage);
+        Index logIndex = parseIndexOrThrowUsage(parts[1], usageMessage);
+
+        return new LogDeleteCommand(personIndex, logIndex);
+    }
+
+    private static Index parseIndexOrThrowUsage(String rawIndex, String usageMessage) throws ParseException {
         try {
-            String trimmedArgs = args.trim();
-            String[] parts = trimmedArgs.split("\\s+");
-
-            if (parts.length != 2) {
-                throw new ParseException(
-                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, LogDeleteCommand.MESSAGE_USAGE));
-            }
-
-            Index personIndex = ParserUtil.parseIndex(parts[0]);
-            Index logIndex = ParserUtil.parseIndex(parts[1]);
-
-            return new LogDeleteCommand(personIndex, logIndex);
+            return ParserUtil.parseIndex(rawIndex);
         } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, LogDeleteCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(usageMessage, pe);
         }
     }
 }
