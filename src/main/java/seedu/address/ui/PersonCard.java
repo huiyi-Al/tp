a6d61ce5bf7fbe7f7547e3c9d5b3d37ee332d459
Ziyue <e1295583@u.nus.edu>
2026-04-01 22:05:28 +0800

    more update

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -34,8 +34,6 @@ public class PersonCard extends UiPart<Region> {
     private Label id;
     @FXML
     private Label phone;
-    @FXML
-    private FlowPane tags;
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
@@ -45,8 +43,5 @@ public class PersonCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
-        person.getTags().stream()
-                .sorted(Comparator.comparing(tag -> tag.tagName))
-                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 }
